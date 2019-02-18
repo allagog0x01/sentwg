fb3319013f21772a9729969148b58eeec8d4c556
btob33 <btob33@proton.com>
2019-02-18 18:40:45 +0530

    bug-fixes

--- sentinel/vpn/__init__.py
+++ sentinel/vpn/__init__.py
@@ -2,7 +2,8 @@
 from .helpers import get_sessions
 from .helpers import update_session_data
 from .wireguard import wireguard
-
+import json
+import logging
 with open("./log_configuration.json", 'r') as logging_configuration_file:
     config_dict = json.load(logging_configuration_file)
 
