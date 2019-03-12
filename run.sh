f70077054694019a47f9052bae05d1a9e34da8e1
Harish <harishmarri551@gmail.com>
2019-03-12 14:47:12 +0530

    path

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 cd /root;
 nohup mongod >> /dev/null & sleep 1;
 gaiacli advanced rest-server --chain-id Sentinel-dev-testnet --node http://209.182.217.171:26657 --home /root/.sentinel & sleep 1;
-cp config /.sentinel/
+cp config /root/.sentinel/
 python app.py
