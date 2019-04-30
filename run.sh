671ffb36e9820c1ed781b230cda522c63a75d59b
allagog <>
2019-04-30 15:25:36 +0530

    updated tm url

--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 cd /root;
 nohup mongod >> /dev/null & sleep 1;
-gaiacli advanced rest-server --chain-id Sentinel-testnet-1.1 --node http://209.182.216.153:26657 --home /root/.sentinel & sleep 1;
+gaiacli advanced rest-server --chain-id Sentinel-testnet-1.1 --node  http://tm-lcd.sentinelgroup.io:26657/  --home /root/.sentinel & sleep 1;
 python app.py
